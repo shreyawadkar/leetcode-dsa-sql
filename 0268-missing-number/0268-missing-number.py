840a589ddd78d304fa8dd87c72ba57e404c2d6d3
shreyawadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-06-01 20:39:17 -0700

    Time: 2705 ms (5.81%), Space: 13.6 MB (12.77%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        b = len(nums)
+        c = []
+        for i in range (0,b+1):
+            c.append(i)
+    
+
+        for j in c:
+            if j in nums:
+                continue
+            else:
+            
+                return j
+        
