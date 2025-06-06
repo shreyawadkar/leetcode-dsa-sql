77c1dca7ffe819691ce2e97b5032be9c7eb7cc73
shreyawadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-06-05 22:30:37 -0700

    Time: 28 ms (18.72%), Space: 15.7 MB (6.67%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,19 +1,14 @@
 class Solution(object):
     def singleNumber(self, nums):
-        element_lists = defaultdict(list)  
-        unique_count = 0  
-        seen = set()  
+        elements = defaultdict(list)
+        seen = set()
 
         for i in nums:
             if i not in seen:
                 seen.add(i)
-                unique_count += 1
-            element_lists[i].append(i)
+            elements[i].append(i)
 
-    
-        for key in element_lists:
-            if len(element_lists[key]) == 1:
-                return key
+        for j in elements:
+            if len(elements[j]) == 1:
+                return j
 
-        
-        
