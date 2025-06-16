5f472be9daa29aa8c8a7d442368c7797baa21ee2
shreyawadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-06-15 19:29:13 -0700

    Time: 7 ms (69.95%), Space: 13.8 MB (31.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -12,6 +12,14 @@ class Solution(object):
                 big_len = len(i)
                 big_key = key
         return(big_key)
+# count = 0
+#     candidate = None
 
+#     for num in nums:
+#         if count == 0:
+#             candidate = num
+#         count += (1 if num == candidate else -1)
+
+#     return candidate
         
         
