950b3d7b2a463fe4fac7fcfbbf9ce33839471273
Shreya Wadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-06-24 20:15:01 -0700

    Time: 0 ms (100%), Space: 13.2 MB (12.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -6,14 +6,12 @@ class Solution(object):
         zero_rows = set()
         zero_cols = set()
 
-     
         for i in range(rows):
             for j in range(cols):
                 if matrix[i][j] == 0:
                     zero_rows.add(i)
                     zero_cols.add(j)
 
-        
         for i in range(rows):
             for j in range(cols):
                 if i in zero_rows or j in zero_cols:
