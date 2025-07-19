8ce4c31d592d5338b360c9155a099d57da6793cd
Shreya Wadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-07-18 19:52:51 -0700

    Time: 7 ms (96.2%), Space: 15.9 MB (82.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,16 +1,10 @@
 class Solution:
     def merge(self, intervals):
-        # Step 1: Sort intervals by starting time
         intervals.sort(key=lambda x: x[0])
-        
         merged = []
-        
         for interval in intervals:
-            # If merged is empty or there's no overlap, just append
             if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
             else:
-                # Overlap: update the end of the last interval
                 merged[-1][1] = max(merged[-1][1], interval[1])
-        
         return merged
