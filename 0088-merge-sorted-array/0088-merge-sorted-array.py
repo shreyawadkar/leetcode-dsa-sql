27116f57072e9e1fa0cced9ff2155b098085088f
Shreya Wadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-07-22 20:01:09 -0700

    Time: 0 ms (100%), Space: 12.5 MB (12.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,12 +2,9 @@ class Solution(object):
     def merge(self, nums1, m, nums2, n):
         
         m1 = min(n, len(nums1))
-    
-    
         for i in range(m1):
             nums1.pop()
-        
-        
+    
         for j in nums2:
             nums1.append(j)
             
