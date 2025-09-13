0ae505da2cc6c4f7e09b379721d5145344bee5c7
Shreya Wadkar <131257358+shreyawadkar@users.noreply.github.com>
2025-09-12 19:10:46 -0700

    Time: 11 ms (54.05%), Space: 12.6 MB (10.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        num = x
+        result = 0
+        while num > 0:
+            ld = num % 10
+            result = (result * 10) + ld
+            num = num // 10
+        return x == result 
+        
+
+        
