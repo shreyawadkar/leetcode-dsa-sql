c53b8eff7011311d070e7472b74227fc8986b1da
Shreya Wadkar <131257358+shreyawadkar@users.noreply.github.com>
2026-09-16 19:01:49 -0700

    Time: 471 ms (85.67%), Space: 12.4 MB (63.96%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,14 +4,12 @@ class Solution(object):
         n = len(nums)
         result = []
 
-        for i in range(n - 3):
-            # Skip duplicates for the first number
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range (n-3):
+            if i > 0 and nums[i] == nums [i - 1]:
                 continue
 
-            for j in range(i + 1, n - 2):
-                # Skip duplicates for the second number
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range (i+ 1, n-2):
+                if j > i + 1 and nums[j] == nums [ j - 1]:
                     continue
 
                 left = j + 1
@@ -19,26 +17,26 @@ class Solution(object):
 
                 while left < right:
                     total = nums[i] + nums[j] + nums[left] + nums[right]
-
                     if total == target:
                         result.append([nums[i], nums[j], nums[left], nums[right]])
 
-                        # Skip duplicates for the third and fourth numbers
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
+                        while left < right and nums[left]==nums[left+1]:
+                            left +=1
 
-                        left += 1
-                        right -= 1
+                        while left < right and nums[right]==nums[right-1]:
+                            right -=1
+                        
+                        left +=1
+                        right -=1
 
                     elif total < target:
-                        left += 1
-                    else:
-                        right -= 1
+                        left +=1
+
+                    else :
+                        right -=1
 
         return result
-
+        
 
             
                 
